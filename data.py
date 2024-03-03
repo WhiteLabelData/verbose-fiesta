b6a9f7209bba99e37182b64a804954bc64869615
WhiteLabelData <161954170+WhiteLabelData@users.noreply.github.com>
2024-03-03 17:33:27 +0000

    connected to Azure!

--- data.py
+++ data.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 # Configure database connection
 server = 'unifidataserver.database.windows.net'
 database = 'unifidata'
-username = 'gavinsbrown@gmail.com'
+username = 'unifidata_admin'
 password = 'Alph4num3r!cal'
-driver = '{ODBC Driver 17 for SQL Server}'
+driver = '{ODBC Driver 18 for SQL Server}'
 
 # Establish database connection
 conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server +
@@ -18,10 +18,21 @@ conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server +
 @app.route('/api/data', methods=['GET'])
 def get_data():
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM YourTable')
-    data = cursor.fetchall()
+    cursor.execute('SELECT * from dbo.process')
+    rows = cursor.fetchall()
+
+    # Convert rows to a list of dictionaries
+    data = []
+    for row in rows:
+        row_dict = {}
+        for key, value in zip(cursor.description, row):
+            if isinstance(value, bytes):
+                value = value.decode('utf-8')  # Convert bytes to string
+            row_dict[key[0]] = value
+        data.append(row_dict)
+
     cursor.close()
     return jsonify({'data': data})
 
-if __name__ == '__data__':
+if __name__ == '__main__':
     app.run(debug=True)
