9b038bc8cd0f7be1acd373e649047471d7e4fa66
WhiteLabelData <161954170+WhiteLabelData@users.noreply.github.com>
2024-03-04 23:13:03 +0000

    refromat

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask 
+from flask import Flask
 
-app = Flask(__name__) 
-  
-@app.route('/') 
+app = Flask(__name__)
 
-def index(): 
+@app.route('/')
+def index():
+    return 'Welcome to my Flask API!'
 
-return 'Welcome to my Flask API!' 
-
-if __name__ == '__main__': 
-
-app.run(debug=True) 
+if __name__ == '__main__':
+    app.run(debug=True)
